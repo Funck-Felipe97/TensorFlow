91991fc81de0c4b987f2d3d90103b02c35d53d4d
Felipe Carvalho Funck <felipe.funck@jlinformatica.net.br>
2019-01-06 14:50:34 -0200

    Constantes com tensorflow

---
+++ sintaxe-basica/constantes.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+valor1 = tf.constant(2)
+valor2 = tf.constant(3)
+
+type(valor1)
+print(valor1)
+
+soma = valor1 + valor2
+type(soma)
+print(soma)
+
+with tf.Session() as sess:
+    s = sess.run(soma)
+
+print(s)
+
+texto1 = tf.constant("Texto 1")
+texto2 = tf.constant("Texto 2")
+type(texto1)
+print(texto1)
+
+with tf.Session() as sess:
+    con = sess.run(texto1 + texto2)
+    
+print(con)
+
